e085d9dc656c12e33ead625565d960a70704e450
ElectroluxV2 <electroluxv2@gmail.com>
2021-06-04 20:30:38 +0200

    Exercise 3

--- src/main/java/org/example/Exercise3.java
+++ src/main/java/org/example/Exercise3.java
@@ -1,14 +1,39 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import static org.example.Exercise1.prepareTable;
 
 public class Exercise3 extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
         var table = prepareTable();
+
+        var button = new Button("Print info about selected");
+        button.setOnMouseClicked(mouseEvent -> {
+            var sb = new StringBuilder();
+            for(Person p: table.getSelectionModel().getSelectedItems()) {
+                sb.append(p.toString());
+                sb.append(System.lineSeparator());
+            }
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Information");
+            alert.setHeaderText("Selected data");
+            alert.setContentText(sb.toString());
+
+            alert.showAndWait();
+        });
+
+        var scene = new Scene(new StackPane(new VBox(button, table)), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
 
--- src/main/java/org/example/README.md
+++ src/main/java/org/example/README.md
@@ -1,13 +1,14 @@
 #Zadanie 1
 Utwórz klasę Person zawierającą imię, nazwisko, płeć(enum) i wiek. Zahardcoduj parę osóbi wyświetl je w klasie TableView.Wskazówka: najpierwutwórz TableColumn dla każdegopola klasy Person.
-
 ![Result](./1.png?raw=true)
 
 #Zadanie 2
 Dodaj przycisk (nad tabelą), którego wciśnięcie wypiszedo konsoli informację ozaznaczonych osobach.
-
 ![Result](./2.png?raw=true)
 
+#Zadanie 3
+Przerób przycisk tak, żeby wyświetlał informację ozaznaczonych osobach w dialogu
+![Result](./3.png?raw=true)
 
 
 
